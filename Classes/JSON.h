14693cd8393465a7c185f11bd75785000a27a18b
Ullrich Schäfer <ullrich@seidbereit.de>
2011-03-04 16:52:20 +0100

    Different header inclusion for static library & framework

--- Classes/JSON.h
+++ Classes/JSON.h
@@ -48,7 +48,7 @@
 // This setting of 1 is best if you copy the source into your project. 
 // The build transforms the 1 to a 0 when building the framework and static lib.
 
-#if 1
+#if TARGET_OS_IPHONE
 
 #import "SBJsonParser.h"
 #import "SBJsonWriter.h"
